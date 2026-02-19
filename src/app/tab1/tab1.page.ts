5f725c99d32a5bab987f9dfa9019d82ce9c9bf62
aneta.kocendova <aneta.kocendova@seznam.cz>
2026-02-19 10:00:10 +0100

    dalsi commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import {IonHeader, IonToolbar, IonTitle, IonContent, IonButton} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from "@angular/common";
+import { IonicModule } from "@ionic/angular";
+import { HttpClientModule } from "@angular/common/http";
+import { MealService } from "../../core/services/meal.service";
 
 @Component({
   selector: 'app-tab1',
+  standalone: true,
+  imports: [IonModule,],
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+
 })
 export class Tab1Page {
   constructor() {}
