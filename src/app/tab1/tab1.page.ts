80418023bd5c86ad8e791620585d3699d0d012e6
aneta.kocendova <aneta.kocendova@seznam.cz>
2026-02-23 09:35:44 +0100

    hlavni stranka - uprava

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,19 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import {IonHeader, IonToolbar, IonTitle, IonContent, IonButton} from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-import { CommonModule } from "@angular/common";
-import { IonicModule } from "@ionic/angular";
-import { HttpClientModule } from "@angular/common/http";
-import { MealService } from "../../core/services/meal.service";
+import { Component, OnInit, inject } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MealService } from 'src/app/core/services/meal.service';
+import { Meal } from 'src/app/models/meal.model';
 
 @Component({
   selector: 'app-tab1',
+  templateUrl: './tab1.page.html',
+  styleUrls: ['./tab1.page.scss'],
   standalone: true,
-  imports: [IonModule,],
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
-
+  imports: [IonicModule, CommonModule, FormsModule],
 })
-export class Tab1Page {
-  constructor() {}
+export class Tab1Page implements OnInit {
+
+  private mealService = inject(MealService);
+
+  recipes: Meal[] = [];
+  filteredRecipes: Meal[] = [];
+  searchTerm: string = '';
+
+  ngOnInit() {
+    this.loadMeals();
+  }
+
+  loadMeals() {
+    this.mealService.getAllMeals().subscribe(res => {
+      this.recipes = res.meals;
+      this.filteredRecipes = res.meals;
+    });
+  }
+
+  filterRecipes() {
+    this.filteredRecipes = this.recipes.filter(r =>
+      r.strMeal.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
 }
