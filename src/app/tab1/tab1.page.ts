5349a25f9b5132afde7c23b8ffcdd72667b78481
Anetka157 <aneta.kocendova@seznam.cz>
2026-03-09 16:58:59 +0100

    uprava prvni stranky

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -4,13 +4,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MealService } from 'src/app/core/services/meal.service';
 import { FilterComponent } from 'src/app/filter/filter.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: './tab1.page.html',
   styleUrls: ['./tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, FilterComponent],
+  imports: [IonicModule, CommonModule, FormsModule, RouterModule],
 })
 export class Tab1Page implements OnInit {
 
